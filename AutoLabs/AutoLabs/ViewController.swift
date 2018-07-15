6d1c95a6406804d80afaedc15a46c043ce68d6a5
akabel92 <ali.kabil92@gmail.com>
2018-07-15 15:18:34 +0200

    add Xcode project

---
+++ AutoLabs/AutoLabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLabs
+//
+//  Created by Ali Kabel on 15.07.18.
+//  Copyright © 2018 Ali Kabel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
