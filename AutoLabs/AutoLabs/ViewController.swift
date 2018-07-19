7c6cf10169d6b4d6a7fd81a80d5f51c6a3f001af
akabel92 <ali.kabil92@gmail.com>
2018-07-19 20:49:57 +0200

    record and recognise speech

--- AutoLabs/AutoLabs/ViewController.swift
+++ AutoLabs/AutoLabs/ViewController.swift
@@ -7,18 +7,70 @@
 //
 
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
 
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+
+    // MARK: - UIView Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Actions
+    
+    @IBAction func recordButtonClicked(_ sender: Any) {
+        recordAndRecognizeSpeech()
+    }
+    
+    // MARK:- Recording Methods
+    
+    func recordAndRecognizeSpeech() {
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat, block: { buffer, _ in
+            self.request.append(buffer)
+        })
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            // show alert view with error
+            return print(error)
+        }
+        
+        guard let myRecognizer = SFSpeechRecognizer() else { return }
+        
+        if !myRecognizer.isAvailable {
+            return
+        }
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                let resultText = result.bestTranscription.formattedString
+                print(resultText)
+                self.cityLabel.text = resultText
+            } else if let error = error {
+                print(error)
+            }
+        })
+    }
+   
 
 
 }
